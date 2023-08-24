b3084bd5f5be3b39c4461f37b27715042c60d2f0
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-08-24 14:02:30 +0100

    corrected linters error

--- modules/addRemove.js
+++ modules/addRemove.js
@@ -1,32 +1,32 @@
-const list = document.querySelector(".to-do-list");
-const addText = document.querySelector(".to-do-add-text");
-const form = document.querySelector(".form");
-const enterBtn = document.querySelector(".enter-btn");
+const list = document.querySelector('.to-do-list');
+const addText = document.querySelector('.to-do-add-text');
+const form = document.querySelector('.form');
+const enterBtn = document.querySelector('.enter-btn');
 
 const wholeList = function () {
-  let getStorage = JSON.parse(localStorage.getItem("list")) || [];
+  let getStorage = JSON.parse(localStorage.getItem('list')) || [];
   const setStorage = function () {
-    localStorage.setItem("list", JSON.stringify(getStorage));
+    localStorage.setItem('list', JSON.stringify(getStorage));
   };
 
   const displayList = function () {
-    list.innerHTML = "";
+    list.innerHTML = '';
     getStorage.forEach((item) => {
       list.innerHTML += `
          <li class="list-task">
             <div class="to-do-list-task">
               <input type="checkbox" class="to-do-input" id= ${
-                item.index - 1
-              } />
+  item.index - 1
+} />
               <p class="to-do-text" id= ${item.index - 1} >${item.desc}</p>
             </div>
             <div class="to-do-btn-del">
               <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${
-                item.index
-              }  ></i>
+  item.index
+}  ></i>
               <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id= ${
-                item.index
-              }></i>
+  item.index
+}></i>
             </div>
           </li>`;
     });
@@ -44,12 +44,12 @@ const wholeList = function () {
       setStorage();
       displayList();
     }
-    addText.value = "";
+    addText.value = '';
   };
 
   // Submit the form
-  form.addEventListener("submit", getList);
-  enterBtn.addEventListener("click", getList);
+  form.addEventListener('submit', getList);
+  enterBtn.addEventListener('click', getList);
 
   // Function for deleting the button
   const removeListIndex = (index) => {
@@ -58,8 +58,8 @@ const wholeList = function () {
     setStorage();
   };
 
-  list.addEventListener("click", (e) => {
-    const target = e.target.closest(".add-trash");
+  list.addEventListener('click', (e) => {
+    const target = e.target.closest('.add-trash');
     if (!target) return;
     removeListIndex(target.id);
     getStorage.forEach((item, i) => {
@@ -71,17 +71,17 @@ const wholeList = function () {
   // Function for editing the list
   const editList = (target) => {
     target.parentElement.previousElementSibling.children[1].setAttribute(
-      "contenteditable",
-      "true"
+      'contenteditable',
+      'true',
     );
     target.parentElement.previousElementSibling.children[1].focus();
-    target.parentElement.parentElement.classList.add("bg-color");
-    target.classList.toggle("hidden");
-    target.nextElementSibling.classList.toggle("hidden");
+    target.parentElement.parentElement.classList.add('bg-color');
+    target.classList.toggle('hidden');
+    target.nextElementSibling.classList.toggle('hidden');
   };
 
-  list.addEventListener("click", (e) => {
-    const target = e.target.closest(".remove-list");
+  list.addEventListener('click', (e) => {
+    const target = e.target.closest('.remove-list');
 
     if (!target) return;
     editList(target);
