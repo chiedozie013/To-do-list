7b85218bb443ac20add4019fe92a2b2bd55377d9
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-08-24 14:01:08 +0100

    Corrected remove function

--- modules/addRemove.js
+++ modules/addRemove.js
@@ -1,26 +1,32 @@
-const list = document.querySelector('.to-do-list');
-const addText = document.querySelector('.to-do-add-text');
-const form = document.querySelector('.form');
-const enterBtn = document.querySelector('.enter-btn');
+const list = document.querySelector(".to-do-list");
+const addText = document.querySelector(".to-do-add-text");
+const form = document.querySelector(".form");
+const enterBtn = document.querySelector(".enter-btn");
 
 const wholeList = function () {
-  let getStorage = JSON.parse(localStorage.getItem('list')) || [];
+  let getStorage = JSON.parse(localStorage.getItem("list")) || [];
   const setStorage = function () {
-    localStorage.setItem('list', JSON.stringify(getStorage));
+    localStorage.setItem("list", JSON.stringify(getStorage));
   };
 
   const displayList = function () {
-    list.innerHTML = '';
+    list.innerHTML = "";
     getStorage.forEach((item) => {
       list.innerHTML += `
          <li class="list-task">
             <div class="to-do-list-task">
-              <input type="checkbox" class="to-do-input" id= ${item.index}/>
-              <p class="to-do-text" id= ${item.index}>${item.desc}</p>
+              <input type="checkbox" class="to-do-input" id= ${
+                item.index - 1
+              } />
+              <p class="to-do-text" id= ${item.index - 1} >${item.desc}</p>
             </div>
             <div class="to-do-btn-del">
-              <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${item.index}  ></i>
-              <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id= ${item.index}></i>
+              <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${
+                item.index
+              }  ></i>
+              <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id= ${
+                item.index
+              }></i>
             </div>
           </li>`;
     });
@@ -38,12 +44,12 @@ const wholeList = function () {
       setStorage();
       displayList();
     }
-    addText.value = '';
+    addText.value = "";
   };
 
   // Submit the form
-  form.addEventListener('submit', getList);
-  enterBtn.addEventListener('click', getList);
+  form.addEventListener("submit", getList);
+  enterBtn.addEventListener("click", getList);
 
   // Function for deleting the button
   const removeListIndex = (index) => {
@@ -52,12 +58,12 @@ const wholeList = function () {
     setStorage();
   };
 
-  list.addEventListener('click', (e) => {
-    const target = e.target.closest('.add-trash');
+  list.addEventListener("click", (e) => {
+    const target = e.target.closest(".add-trash");
     if (!target) return;
     removeListIndex(target.id);
     getStorage.forEach((item, i) => {
-      item.index = i + 1;
+      item.index = i;
     });
     setStorage();
   });
@@ -65,32 +71,20 @@ const wholeList = function () {
   // Function for editing the list
   const editList = (target) => {
     target.parentElement.previousElementSibling.children[1].setAttribute(
-      'contenteditable',
-      'true',
+      "contenteditable",
+      "true"
     );
     target.parentElement.previousElementSibling.children[1].focus();
-    target.parentElement.parentElement.classList.add('bg-color');
-    target.classList.toggle('hidden');
-    target.nextElementSibling.classList.toggle('hidden');
+    target.parentElement.parentElement.classList.add("bg-color");
+    target.classList.toggle("hidden");
+    target.nextElementSibling.classList.toggle("hidden");
   };
 
-  list.addEventListener('click', (e) => {
-    const target = e.target.closest('.remove-list');
+  list.addEventListener("click", (e) => {
+    const target = e.target.closest(".remove-list");
 
     if (!target) return;
     editList(target);
-  });
-
-  list.addEventListener('focusout', (e) => {
-    const target = e.target.closest('.to-do-text');
-    const targetID = Number(target.id - 1);
-    if (!target) return;
-    if (target) {
-      getStorage[targetID].desc = target.textContent;
-      target.parentElement.parentElement.classList.remove('bg-color');
-      setStorage();
-      window.location.reload();
-    }
   });
 
   displayList();
