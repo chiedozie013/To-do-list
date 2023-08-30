6b19f520f6858542f4c19ee123a6772fcbbb322d
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-08-30 12:49:18 +0100

    linters error correction

--- modules/addRemove.js
+++ modules/addRemove.js
@@ -1,14 +1,14 @@
-const list = document.querySelector(".to-do-list");
-const addText = document.querySelector(".to-do-add-text");
-const form = document.querySelector(".form");
-const enterBtn = document.querySelector(".enter-btn");
-const clearBtn = document.querySelector(".to-do-clear");
-const getLocalStorage = () => JSON.parse(localStorage.getItem("list")) || [];
+const list = document.querySelector('.to-do-list');
+const addText = document.querySelector('.to-do-add-text');
+const form = document.querySelector('.form');
+const enterBtn = document.querySelector('.enter-btn');
+const clearBtn = document.querySelector('.to-do-clear');
+const getLocalStorage = () => JSON.parse(localStorage.getItem('list')) || [];
 const displayList = () => {
-  document.querySelector(".to-do-list").innerHTML = "";
+  document.querySelector('.to-do-list').innerHTML = '';
   const getStorage = getLocalStorage();
   getStorage.forEach((item) => {
-    document.querySelector(".to-do-list").innerHTML += `
+    document.querySelector('.to-do-list').innerHTML += `
       <li class="list-task">
           <div class="to-do-list-task">
             <input type="checkbox" class="to-do-input" id= ${item.index - 1} />
@@ -16,18 +16,18 @@ const displayList = () => {
           </div>
           <div class="to-do-btn-del">
             <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${
-              item.index
-            }  ></i>
+  item.index
+}  ></i>
             <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id="${
-              item.index
-            }"></i>
+  item.index
+}"></i>
           </div>
         </li>`;
   });
   // return getStorage;
 };
 const setStorage = (data) => {
-  localStorage.setItem("list", JSON.stringify(data));
+  localStorage.setItem('list', JSON.stringify(data));
 };
 const addList = (task) => {
   const getStorage = getLocalStorage();
@@ -51,19 +51,19 @@ const removeListIndex = (index) => {
 const wholeList = () => {
   // let getStorage = JSON.parse(localStorage.getItem('list')) || [];
   // Submit the form
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     addList(addText.value);
-    addText.value = "";
+    addText.value = '';
   });
-  enterBtn.addEventListener("click", (e) => {
+  enterBtn.addEventListener('click', (e) => {
     e.preventDefault();
     addList(addText.value);
-    addText.value = "";
+    addText.value = '';
   });
-  list.addEventListener("click", (e) => {
+  list.addEventListener('click', (e) => {
     const getStorage = getLocalStorage();
-    const target = e.target.closest(".add-trash");
+    const target = e.target.closest('.add-trash');
     if (!target) return;
     removeListIndex(target.id);
     getStorage.forEach((item, i) => {
@@ -74,62 +74,62 @@ const wholeList = () => {
   // Function for editing the list
   const editList = (target) => {
     target.parentElement.previousElementSibling.children[1].setAttribute(
-      "contenteditable",
-      "true"
+      'contenteditable',
+      'true',
     );
     target.parentElement.previousElementSibling.children[1].focus();
-    target.parentElement.parentElement.classList.add("bg-color");
-    target.classList.toggle("hidden");
-    target.nextElementSibling.classList.toggle("hidden");
+    target.parentElement.parentElement.classList.add('bg-color');
+    target.classList.toggle('hidden');
+    target.nextElementSibling.classList.toggle('hidden');
   };
-  list.addEventListener("click", (e) => {
-    const target = e.target.closest(".remove-list");
+  list.addEventListener('click', (e) => {
+    const target = e.target.closest('.remove-list');
     if (!target) return;
     editList(target);
   });
   // Function to implement when focused out on list
-  list.addEventListener("focusout", (e) => {
-    const target = e.target.closest(".to-do-text");
+  list.addEventListener('focusout', (e) => {
+    const target = e.target.closest('.to-do-text');
     const targetID = Number(e.target.id);
     if (!target) return;
     if (target) {
       const getStorage = getLocalStorage();
       getStorage[targetID].desc = target.textContent;
-      target.parentElement.parentElement.classList.remove("bg-color");
+      target.parentElement.parentElement.classList.remove('bg-color');
       setStorage(getStorage);
       window.location.reload();
     }
   });
   // Function to check box
-  list.addEventListener("change", (e) => {
-    const target = e.target.closest(".to-do-input");
+  list.addEventListener('change', (e) => {
+    const target = e.target.closest('.to-do-input');
     const text = e.target.nextElementSibling;
     const targetID = Number(e.target.id);
     if (!target) return;
     if (target.checked) {
       const getStorage = getLocalStorage();
-      text.style.textDecoration = "line-through";
+      text.style.textDecoration = 'line-through';
       getStorage[targetID].completed = true;
       setStorage(getStorage);
     } else {
       const getStorage = getLocalStorage();
-      text.style.textDecoration = "none";
+      text.style.textDecoration = 'none';
       getStorage[targetID].completed = false;
       setStorage(getStorage);
     }
   });
   const domLoaded = () => {
     const getStorage = getLocalStorage();
-    const text = document.querySelectorAll(".to-do-text");
-    const checkbox = [...document.querySelectorAll(".to-do-input")];
+    const text = document.querySelectorAll('.to-do-text');
+    const checkbox = [...document.querySelectorAll('.to-do-input')];
     getStorage.forEach((item, i) => {
       if (item.completed === true) {
         checkbox[i].checked = true;
-        text[i].style.textDecoration = "line-through";
+        text[i].style.textDecoration = 'line-through';
       }
     });
   };
-  window.addEventListener("DOMContentLoaded", domLoaded);
+  window.addEventListener('DOMContentLoaded', domLoaded);
   // Function to delete all completed tasks.
   const delBtn = () => {
     let getStorage = getLocalStorage();
@@ -140,7 +140,7 @@ const wholeList = () => {
     setStorage(getStorage);
     displayList();
   };
-  clearBtn.addEventListener("click", delBtn);
+  clearBtn.addEventListener('click', delBtn);
   displayList();
 };
 export { addList, removeListIndex };
